f2d95e09eca208069341979a2ca5f3c19348e494
Birna Bryndís <thorkelsdottir@gmail.com>
2018-03-22 11:54:13 +0100

    routing for frontapage is in

--- cookbook/src/app/app-routing.module.ts
+++ cookbook/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AllRecipesComponent } from './all-recipes/all-recipes.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'all-recipes', component: AllRecipesComponent },
+  { path: 'add-recipe',      component: AddRecipeComponent },
+  { path: 'login',      component: LoginComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- cookbook/src/app/app.module.ts
+++ cookbook/src/app/app.module.ts
@@ -4,18 +4,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { LoginComponent } from './login/login.component';
+import { AllRecipesComponent } from './all-recipes/all-recipes.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { AdminComponent } from './admin/admin.component';
+import { MyRecipesComponent } from './admin/my-recipes/my-recipes.component';
+import { MyProfileComponent } from './admin/my-profile/my-profile.component';
+import { MyFavoritesComponent } from './admin/my-favorites/my-favorites.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AllRecipesComponent,
+    AddRecipeComponent,
+    AdminComponent,
+    MyRecipesComponent,
+    MyProfileComponent,
+    MyFavoritesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule, 
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
